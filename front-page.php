188c939a1ebf8251616fcc119f734d224125858c
ashrafbd1496 <asraf1496@gmail.com>
2022-06-26 10:28:39 +0600

    fixed the enqueue scripts

---
+++ front-page.php
@@ -0,0 +1,22 @@
+<?php 
+/**
+ * Template Name: Front Page
+ * @package personal
+ */
+get_header(); ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php get_footer(); ?>
--- inc/personal-enqueue-scripts.php
+++ inc/personal-enqueue-scripts.php
@@ -21,10 +21,10 @@ class Personal_Enqueue_Scripts {
 		// // bootstrap stylesheet.
 		wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/assets/vendor/bootstrap/css/bootstrap.min.css', [], null );
 		wp_enqueue_style( 'bootstrap-icon', get_template_directory_uri() . '/assets/vendor/bootstrap-icons/bootstrap-icons.css', [], null );
-		wp_enqueue_style( 'boxicons', get_template_directory_uri() . '/assets/vendor/bootstrap-icons/boxicons.min.css', [], null );
-		wp_enqueue_style( 'glightbox', get_template_directory_uri() . '/assets/vendor/bootstrap-icons/glightbox.min.css', [], null );
-		wp_enqueue_style( 'remixicon', get_template_directory_uri() . '/assets/vendor/bootstrap-icons/remixicon.css', [], null );
-		wp_enqueue_style( 'swiper-bundle', get_template_directory_uri() . '/assets/vendor/bootstrap-icons/swiper-bundle.min.css', [], null );
+		wp_enqueue_style( 'boxicons', get_template_directory_uri() . '/assets/vendor/boxicons/css/boxicons.min.css', [], null );
+		wp_enqueue_style( 'glightbox', get_template_directory_uri() . '/assets/vendor/glightbox/css/glightbox.min.css', [], null );
+		wp_enqueue_style( 'remixicon', get_template_directory_uri() . '/assets/vendor/remixicon/remixicon.css', [], null );
+		wp_enqueue_style( 'swiper-bundle', get_template_directory_uri() . '/assets/vendor/swiper/swiper-bundle.min.css', [], null );
 
 		// // Fontawesome V4 stylesheet.
 		// wp_enqueue_style( 'fontawesome-5', get_template_directory_uri() . '/assets/css/all.min.css', [], null );
@@ -43,15 +43,16 @@ class Personal_Enqueue_Scripts {
 		/**
 		 * Load All jQuery Library
 		 */
-		wp_enqueue_script( 'purecounter', get_template_directory_uri() . 'assets/vendor/purecounter/purecounter.js', [], _S_VERSION, true );
-		wp_enqueue_script( 'bootstrap-bundle', get_template_directory_uri() . 'assets/vendor/purecounter/bootstrap.bundle.min.js.js', [], _S_VERSION, true );
-		wp_enqueue_script( 'glightbox', get_template_directory_uri() . 'assets/vendor/purecounter/glightbox.min.js', [], _S_VERSION, true );
-		wp_enqueue_script( 'isotope', get_template_directory_uri() . 'assets/vendor/purecounter/isotope.pkgd.min.js', [], _S_VERSION, true );
-		wp_enqueue_script( 'swiper-bundle', get_template_directory_uri() . 'assets/vendor/purecounter/swiper-bundle.min.js', [], _S_VERSION, true );
-		wp_enqueue_script( 'noframework.waypoints', get_template_directory_uri() . 'assets/vendor/purecounter/noframework.waypoints.js', [], _S_VERSION, true );
+		wp_enqueue_script( 'purecounter', get_template_directory_uri() . '/assets/vendor/purecounter/purecounter.js', [], _S_VERSION, true );
+		wp_enqueue_script( 'bootstrap-bundle', get_template_directory_uri() . '/assets/vendor/bootstrap/js/bootstrap.bundle.min.js', [], _S_VERSION, true );
+		wp_enqueue_script( 'glightbox', get_template_directory_uri() . '/assets/vendor/glightbox/js/glightbox.min.js', [], _S_VERSION, true );
+		wp_enqueue_script( 'isotope', get_template_directory_uri() . '/assets/vendor/isotope-layout/isotope.pkgd.min.js', [], _S_VERSION, true );
+		wp_enqueue_script( 'swiper-bundle', get_template_directory_uri() . '/assets/vendor/swiper/swiper-bundle.min.js', [], _S_VERSION, true );
+		wp_enqueue_script( 'noframework.waypoints', get_template_directory_uri() . '/assets/vendor/waypoints/noframework.waypoints.js', [], _S_VERSION, true );
 		wp_enqueue_script( 'email-form-validate', get_template_directory_uri() . '/assets/vendor/php-email-form/validate.js', [], _S_VERSION, true );
 
-		wp_enqueue_script( 'personal-navigation', get_template_directory_uri() . '/assets/js/navigation.js', [], _S_VERSION, true );
+		wp_enqueue_script( 'personal-customizer', get_template_directory_uri() . '/js/customizer.js', ['jquery','customize-preview'], _S_VERSION, true );
+		wp_enqueue_script( 'personal-navigation', get_template_directory_uri() . '/js/navigation.js', [], _S_VERSION, true );
 
 		// Add personal-main js library
 		wp_enqueue_script( 'personal-template-main', get_template_directory_uri() . '/assets/js/main.js', ['jquery'], '', true );
